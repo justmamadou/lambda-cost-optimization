20b908fa08749e0f99fb26c2c3f63fd3149bcd7d
iam_mamadou <66375343+justmamadou@users.noreply.github.com>
2025-01-10 12:16:54 +0000

    Update and rename ec2.tf to 05_ec2.tf

---
+++ 05_ec2.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+}
+
+resource "aws_instance" "red" {
+  ami           = data.aws_ami.ubuntu.id
+  count         = 2
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Red"
+  }
+}
+
+resource "aws_instance" "green" {
+  ami           = data.aws_ami.ubuntu.id
+  count         = 2
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Red"
+  }
+}
